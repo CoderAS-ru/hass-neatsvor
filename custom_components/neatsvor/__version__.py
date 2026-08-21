e0b35dd193ec9ca0968e71fe94f78d964e9daf09
Aleksandr Skorikov <195150937+CoderAS-ru@users.noreply.github.com>
2026-08-21 17:32:06 +0300

    Delete custom_components/neatsvor directory

--- custom_components/neatsvor/__version__.py
+++
@@ -1,4 +0,0 @@
-"""Version information for Neatsvor integration."""
-
-__version__ = "2.1.0"
-__release_date__ = "2026-08-21"
--- custom_components/neatsvor/liboshome/rest/__init__.py
+++
@@ -1,7 +0,0 @@
-"""REST API module for Neatsvor."""
-
-from custom_components.neatsvor.liboshome.rest.async_client import NeatsvorRestAsync
-
-__all__ = [
-    'NeatsvorRestAsync'
-]
